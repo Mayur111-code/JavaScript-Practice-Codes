3f2d5d3771a2b224eaba7b4adc32951765b5af04
Mayur Borse 👨🏻‍💻 <mayurborse7440@gmail.com>
2025-07-11 15:55:16 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,8 @@
+// to the start the Javascript Module 
+// Lets Start
+// basic js output method:
+
+alert("Hello everyone! Start The Js ");
+// console.log(1+1)
+// console.log(1+2+'2')
+// console.log('3'+1+4)
