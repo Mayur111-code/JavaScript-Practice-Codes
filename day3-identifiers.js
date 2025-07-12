d02766e7051f82e593de8e4c46bc4e0078537c56
Mayur Borse 👨🏻‍💻 <mayurborse7440@gmail.com>
2025-07-13 00:28:51 +0530

    Add files via upload

---
+++ day3-identifiers.js
@@ -0,0 +1,28 @@
+var num1 = 30;
+        console.log(num1);
+        var myNum = 100;
+        console.log(myNum);
+        var mayur = 40;
+        console.log(mayur);
+
+        var totalstudents = 30; //variable withot spaces
+        var TotalStudents = 20; //PascalCasing name 
+        var totalStudents = 40; // camelCasing name
+        var total_student = 70;   // use undersore (_) to the variable
+
+          
+
+        //note : not use any symbol only allow 2 symbols that is ($),(_) only use the symbols.
+        // not make variables in like : var 1mark = 30; it is not allwo in write the variables (example: var 123 = 67; , var 12yash = 67 )
+
+
+        // use like this types is allwo (var  ma23ks = 70; )
+         
+
+        var mark2 = 90;
+        console.log(mark2);
+         
+
+        var king23s = 45;
+        console.log(king23s);
+
---
+++ day3-variables.js
@@ -0,0 +1,26 @@
+
+
+// Variables
+
+//     Variables are the containers which are used to store some data which can be later used for processing.
+//     The variables are made up of two parts:
+//     1. Declarator
+//     2. Identifier
+
+var n1 = 100;   // var n1 = variable and 100 is data
+console.log(n1);
+if (n1 % 2 == 0){
+    console.log("The number is even");
+} else {
+    console.log("The number is odd");
+
+}
+
+
+let num = 12;
+console.log(num);
+if (num >= 18){
+    console.log("vote");
+} else {
+    console.log("not vote");
+}
