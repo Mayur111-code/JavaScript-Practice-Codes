9d718400ed343ba89dd2c3cc457712d7a9455c87
Mayur Borse 👨🏻‍💻 <mayurborse7440@gmail.com>
2025-07-22 23:35:10 +0530

    Add files via upload

---
+++ typecasting.js
@@ -0,0 +1,47 @@
+
+        // type casting is the convert the datatype into the number
+
+        //modify method 
+
+        //let num1 = +prompt("Enter first number");
+        //let num2 =  +prompt("Enter secound number");
+
+       // console.log(num1 + " this type is " + typeof num1);
+        //console.log(num2 + " this type is " + typeof num2);
+
+let myNum1 = 90;
+let myNum2 = 100;
+
+
+
+console.log("*****************************************");
+
+// number method 
+
+
+let num3 = Number (12);
+let num4 = Number (23);
+console.log(num3,num4);
+console.log(typeof num3 , typeof num4);
+console.log("**************************************");
+
+let num5 = parseInt(myNum1)
+let num6 = parseInt(myNum2);
+
+console.log(num5 , num6);
+
+console.log(typeof num5 , typeof num6);
+
+
+
+/*
+
+output : ==>
+****************************************
+12 23
+number number
+**************************************
+90 100
+number number
+
+*/
